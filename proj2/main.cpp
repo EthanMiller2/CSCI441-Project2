1c9e185d75e87f420b61ec04a594431a2dd16751
AlexPelaez <Alex@192.168.1.7>
2020-04-24 14:14:57 -0400

    Cleaned up code and it works now

--- proj2/main.cpp
+++ proj2/main.cpp
@@ -13,47 +13,6 @@
 #include "shape.h"
 #include "timer.h"
 
-class AABB
-{ 
-private:
-    float minX;
-    float maxX;
-    float minY;
-    float maxY;
-
-public: 
-    AABB(float a, float b, float c, float d) { 
-        minX = a;
-        maxX = b;
-        minY = c;
-        maxY = d;
-    } 
-
-    bool intersect(const Ray &r){
-        float tmin = (minX - r.origin.x) / r.direction.x; 
-        float tmax = (maxX - r.origin.x) / r.direction.x; 
- 
-        if (tmin > tmax) std::swap(tmin, tmax); 
- 
-        float tymin = (minY - r.origin.y) / r.direction.y; 
-        float tymax = (maxY - r.origin.y) / r.direction.y; 
- 
-        if (tymin > tymax) std::swap(tymin, tymax); 
- 
-        if ((tmin > tymax) || (tymin > tmax)) 
-            return false; 
- 
-        if (tymin > tmin) 
-            tmin = tymin; 
- 
-        if (tymax < tmax) 
-            tmax = tymax; 
-
-        return true; 
-    } 
-       
-}; 
-
 class BruteForceIntersector : public Intersector {
 public:
 
@@ -69,34 +28,6 @@ public:
         return hit;
     }
 };
-
-// std::vector<AABB> boundingBoxes;
-// std::vector<AABB> boundingBoxes1;
-// std::vector<AABB> boundingBoxes2;
-
-
-// class MySlickIntersector : public Intersector {
-// public:
-//     Hit find_first_intersection(const World& world, const Ray& ray) {
-//         Hit hit(ray);
-//         for(auto box: boundingBoxes2){
-//             if(box.intersect(ray)){
-//                 for (auto surface : world.shapes()) {
-//                     double cur_t = surface->intersect(ray);
-
-//                     if (cur_t < hit.t()) {
-//                         hit.update(surface, cur_t);
-//                     }
-//                 }
-
-//             }
-
-//         }
-//         return hit;
-//     }
-// };
-
-
 
 double rand_val() {
     static bool init = true;
@@ -126,39 +57,14 @@ std::vector<Triangle> random_box() {
     return Obj::make_box(glm::vec3(x, y, z), scale, rand_color());
 }
 
-// float getMax(float a, float b, float c){
-//     // std::cout << a << ":" << b << ":" << c << std::endl;
-//     // std::cout << "Max: " << fmax(fmax(a,b),c) << std::endl;
-//     return(fmax(fmax(a,b),c));
-
-// }
-
-// float getMin(float a, float b, float c){
-//     // std::cout << a << ":" << b << ":" << c << std::endl;
-//     // std::cout << "Min: " << fmin(fmin(a,b),c) << std::endl;
-//     return(fmin(fmin(a,b),c));
-
-// }
-
-
-
-struct thread_data {
-   int  thread_id;
-   char *message;
-};
-
 int main(int argc, char** argv) {
-    
+    // create an image imageWidth pixels wide by imageHeight pixels tall
+    // int imageWidth = 320;
+    // int imageHeight = 240;
+    int imageWidth = 640;
+    int imageHeight = 480;
 
-    // set the number of boxes
-    int NUM_BOXES = 4;
-
-    // create an image 640 pixels wide by 480 pixels tall
-    int imageWidth = 320;
-    int imageHeight = 240;
-    // int imageWidth = 640;
-    // int imageHeight = 480;
-
+    // set up the images
     bitmap_image image(imageWidth*4, imageHeight*4);
     bitmap_image image2(imageWidth, imageHeight);
     bitmap_image image3(imageWidth, imageHeight);
@@ -176,9 +82,6 @@ int main(int argc, char** argv) {
     );
 
     // setup lights
-    // see http://wiki.ogre3d.org/tiki-index.php?page=-Point+Light+Attenuation
-    // for good attenuation value.
-    // I found the values at 7 to be nice looking
     PointLight l1(glm::vec3(1, 1, 1), glm::vec3(3, -3, 0), 1.0, .7, 0.18);
     DirectionalLight l2(glm::vec3(.5, .5, .5), glm::vec3(-5, 4, -1));
     Lights lights = { &l1, &l2 };
@@ -186,89 +89,29 @@ int main(int argc, char** argv) {
     // setup world
     World world;
 
-    // add the light
+    // add the light to the world
     world.append(Sphere(l1.position(), .25, glm::vec3(1,1,1)));
 
-
-
-
-    //Bounding boxes around spheres for triangle hierarchical bounding boxes and the spheres
+    // create spheres to be added to the world
     world.append(Sphere(glm::vec3(1, 1, 1), 1, rand_color()));
-    // AABB sphere1 = AABB((1 - 1), (1+1), (1-1), (1+1));
-    // boundingBoxes.push_back(sphere1);
     world.append(Sphere(glm::vec3(2, 2, 4), 2, rand_color()));
-    // AABB sphere2 = AABB((2 - 2), (2+2), (2-2), (2+2));
-    // boundingBoxes.push_back(sphere2);
     world.append(Sphere(glm::vec3(3, 3, 6), 3, rand_color()));
-
     world.append(Sphere(glm::vec3(2, 1, 1), 1, rand_color()));
-    // AABB sphere1 = AABB((1 - 1), (1+1), (1-1), (1+1));
-    // boundingBoxes.push_back(sphere1);
     world.append(Sphere(glm::vec3(5, 6, 4), 2, rand_color()));
-    // AABB sphere2 = AABB((2 - 2), (2+2), (2-2), (2+2));
-    // boundingBoxes.push_back(sphere2);
     world.append(Sphere(glm::vec3(7, 3, 6), 3, rand_color()));
-
     world.append(Sphere(glm::vec3(-3, 6, 1), 1, rand_color()));
-    // // AABB sphere1 = AABB((1 - 1), (1+1), (1-1), (1+1));
-    // // boundingBoxes.push_back(sphere1);
     world.append(Sphere(glm::vec3(-5, -6, 4), 2, rand_color()));
-    // // AABB sphere2 = AABB((2 - 2), (2+2), (2-2), (2+2));
-    // // boundingBoxes.push_back(sphere2);
-    // world.append(Sphere(glm::vec3(-7, 3, 6), 3, rand_color()));
-    // AABB sphere3 = AABB((3 - 3), (3+3), (3-3), (3+3));
-    // boundingBoxes.push_back(sphere3);
+ 
+    // create cubes to be added to the world
+    std::vector<Triangle> cube1 = Obj::make_box(glm::vec3(3, 3, 3), 3, rand_color());
+    std::vector<Triangle> cube2 = Obj::make_box(glm::vec3(-3, 3, 3), 3, rand_color());
+    world.append(cube1);
+    world.append(cube2);
 
-
-
-    //Bounding boxes around spheres for full box hierarchical bounding boxes
-    // world.append(Sphere(glm::vec3(0, 0, 4), 4, rand_color()));
-    // AABB sphere1 = AABB((1 - 1), (1+1), (1-1), (1+1));
-    // boundingBoxes2.push_back(sphere1);
-    // world.append(Sphere(glm::vec3(2, 2, 4), 2, rand_color()));
-    // AABB sphere2 = AABB((2 - 2), (2+2), (2-2), (2+2));
-    // boundingBoxes2.push_back(sphere2);
-    // world.append(Sphere(glm::vec3(3, 3, 6), 3, rand_color()));
-    // AABB sphere3 = AABB((3 - 3), (3+3), (3-3), (3+3));
-    // boundingBoxes2.push_back(sphere3);
-
-
-    // //Bounding boxes for uniform space partinioning
-    // AABB hardCode1 = AABB(0, 160, 0, 480);
-    // boundingBoxes1.push_back(hardCode1);
-
-    // AABB hardCode2 = AABB(160, 320, 0, 480);
-    // boundingBoxes1.push_back(hardCode2);
-
-    // AABB hardCode3 = AABB(320, 480, 0, 480);
-    // boundingBoxes1.push_back(hardCode3);
-
-    // AABB hardCode4 = AABB(480, 640, 0, 480);
-    // boundingBoxes1.push_back(hardCode4);
-
-    // and add some boxes and prep world for rendering
-    // for (int i = 0 ; i < NUM_BOXES ; ++i) {
-    //     std::vector<Triangle> cube = random_box();
-    //     // for(auto t : cube){
-    //     //     float minX = getMin(t.getA().x, t.getB().x, t.getC().x);
-    //     //     float maxX = getMax(t.getA().x, t.getB().x, t.getC().x);
-    //     //     float minY = getMin(t.getA().y, t.getB().y, t.getC().y);
-    //     //     float maxY = getMax(t.getA().y, t.getB().y, t.getC().y);
-    //     //     AABB box = AABB(minX, maxX, minY, maxY);
-    //     //     boundingBoxes.push_back(box);
-    //     // }
-
-    //     world.append(cube);
-    // }
-
-
-    // std::vector<Triangle> cube1 = Obj::make_box(glm::vec3(3, 3, 3), 3, rand_color());
-    // std::vector<Triangle> cube2 = Obj::make_box(glm::vec3(-3, 3, 3), 3, rand_color());
-    // world.append(cube1);
-    // world.append(cube2);
+    // lock the world as no other objects will be added to it
     world.lock();
 
-    // create the intersector
+    // create the intersectors
     BruteForceIntersector intersector1;
     BruteForceIntersector intersector2;
     BruteForceIntersector intersector3;
@@ -278,43 +121,42 @@ int main(int argc, char** argv) {
     BruteForceIntersector intersector7;
     BruteForceIntersector intersector8;
 
-    // MySlickIntersector intersector1;
+    // Start timer 
     Timer timer;
     timer.start();
 
-
-    // and setup the renderer
-
+    // Setup the renderers
     Renderer renderer1(&intersector1, image3, camera, lights, world, 1);
     Renderer renderer2(&intersector2, image3, camera, lights, world, 2);
     Renderer renderer3(&intersector3, image3, camera, lights, world, 3);
     Renderer renderer4(&intersector4, image3, camera, lights, world, 4);
-
-    pthread_t t1;
-    pthread_t t2;
-    pthread_t t3;
-    pthread_t t4;
-
     Renderer renderer5(&intersector5, image, camera, lights, world, 1);
     Renderer renderer6(&intersector6, image, camera, lights, world, 2);
     Renderer renderer7(&intersector7, image, camera, lights, world, 3);
     Renderer renderer8(&intersector8, image, camera, lights, world, 4);
+    
+
+    // create threads and render images
+    pthread_t t1;
+    pthread_t t2;
+    pthread_t t3;
+    pthread_t t4;
     pthread_t t5;
     pthread_t t6;
     pthread_t t7;
     pthread_t t8;
-
-    // create threads and render
     
-    pthread_create(&t1, NULL, &Renderer::hello_helper, &renderer1);
-    pthread_create(&t2, NULL, &Renderer::hello_helper, &renderer2);
-    pthread_create(&t3, NULL, &Renderer::hello_helper, &renderer3);
-    pthread_create(&t4, NULL, &Renderer::hello_helper, &renderer4); 
-    pthread_create(&t5, NULL, &Renderer::hello_helper1, &renderer5);
-    pthread_create(&t6, NULL, &Renderer::hello_helper1, &renderer6);
-    pthread_create(&t7, NULL, &Renderer::hello_helper1, &renderer7);
-    pthread_create(&t8, NULL, &Renderer::hello_helper1, &renderer8);
+    pthread_create(&t1, NULL, &Renderer::r_helper, &renderer1);
+    pthread_create(&t2, NULL, &Renderer::r_helper, &renderer2);
+    pthread_create(&t3, NULL, &Renderer::r_helper, &renderer3);
+    pthread_create(&t4, NULL, &Renderer::r_helper, &renderer4); 
+    pthread_create(&t5, NULL, &Renderer::r_helper, &renderer5);
+    pthread_create(&t6, NULL, &Renderer::r_helper, &renderer6);
+    pthread_create(&t7, NULL, &Renderer::r_helper, &renderer7);
+    pthread_create(&t8, NULL, &Renderer::r_helper, &renderer8);
 
+    // ensure that threads have completed the render before 
+    // starting anti-aliasing process
     (void) pthread_join(t1, NULL);
     (void) pthread_join(t2, NULL);
     (void) pthread_join(t3, NULL);
@@ -324,16 +166,7 @@ int main(int argc, char** argv) {
     (void) pthread_join(t7, NULL);
     (void) pthread_join(t8, NULL);
 
-    std::cout << "Image1: " <<  std::endl;
-    // renderer.render(image3, camera, lights, world);
-    // pthread_t t;
-    // pthread_create(&t, NULL, &Renderer::hello_helper, &renderer);
-    
-
-    // bitmap_image image("ray-traced.bmp");
-
-    // image.save_image("ray-traced.bmp");
-    
+    // Anti-aliasing process!
     int image2i = 0;
     int image2j = 0;
     const unsigned int height = image.height();
@@ -342,9 +175,6 @@ int main(int argc, char** argv) {
     for(std::size_t i = 0; i < height - 2; i+=2){
         image2j = 0;
         for(std::size_t j = 0; j < width - 2; j+=2){
-            // 
-            // std::cout << "Image2: " << image2i / 2 << " : " << image2j / 2 << std::endl;
-
             rgb_t colour1;
             rgb_t colour2;
             rgb_t colour3;
@@ -382,8 +212,13 @@ int main(int argc, char** argv) {
         image2i +=1;
     }
 
+    // End timer
     timer.stop();
+
+    // save temp image used for AA if you like
     image.save_image("temp.bmp");
+
+    // Save images!
     image3.save_image("non-aliased.bmp");
     image2.save_image("anti-alias-image.bmp");
     std::cout << "Rendered in " <<  timer.total() << " milliseconds" << std::endl;
